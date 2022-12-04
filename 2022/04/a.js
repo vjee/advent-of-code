c1bc18368356f3902b3437454ba9a6157e7c419f
vjee <vjee2e@gmail.com>
2022-12-04 07:21:15 +0100

    2022/04

---
+++ 2022/04/a.js
@@ -0,0 +1,27 @@
+const input = require("node:fs").readFileSync("./input.txt", "utf-8");
+
+const pairs = input
+  .split("\n")
+  .map((pair) =>
+    pair
+      .split(",")
+      .map((assignment) => assignment.split("-").map((id) => parseInt(id, 10)))
+  );
+
+let overlapCount = 0;
+
+const fill = ([a, b]) => {
+  let array = [];
+  for (let i = a; i <= b; i++) array.push(i);
+  return array;
+};
+
+for (let i = 0; i < pairs.length; i++) {
+  const [a1, a2] = pairs[i]
+    .map((assigmnent) => fill(assigmnent))
+    .sort((a, b) => a.length - b.length);
+
+  if (a1.every((id) => a2.includes(id))) overlapCount++;
+}
+
+console.log(overlapCount);
